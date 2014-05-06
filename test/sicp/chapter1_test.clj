943ade57277f8cec5134e1c3e2fee8b8f03c64a0
Jingjing Duan <jduan@twitter.com>
2014-05-05 22:45:42 -0700

    format chapter1_test.clj

--- test/sicp/chapter1_test.clj
+++ test/sicp/chapter1_test.clj
@@ -7,43 +7,43 @@
 (def b 4)
 (deftest a-test
   (testing "Exercise 1.1"
-           (is (= 10 10))
-           (is (= 12 (+ 5 3 4)))
-           (is (= 8 (- 9 1)))
-           (is (= 3 (/ 6 2)))
-           (is (= 6 (+ (* 2 4) (- 4 6))))
-           (is (= 19 (+ a b (* a b))))
-           (is (= false (= a b)))
-           (is (= 4 (if (and (> b a) (< b (* a b)))
-                      b
-                      a)))
-           (is (= 16 (cond (= a 4) 6
-                           (= b 4) (+ 6 7 a)
-                           :else 25)))
-           (is (= 6 (+ 2 (if (> b a) b a))))
-           (is (= 16 (* (cond (> a b) a
-                              (< a b) b
-                              :else -1)
-                        (+ a 1))))
-           )
+    (is (= 10 10))
+    (is (= 12 (+ 5 3 4)))
+    (is (= 8 (- 9 1)))
+    (is (= 3 (/ 6 2)))
+    (is (= 6 (+ (* 2 4) (- 4 6))))
+    (is (= 19 (+ a b (* a b))))
+    (is (= false (= a b)))
+    (is (= 4 (if (and (> b a) (< b (* a b)))
+               b
+               a)))
+    (is (= 16 (cond (= a 4) 6
+                    (= b 4) (+ 6 7 a)
+                    :else 25)))
+    (is (= 6 (+ 2 (if (> b a) b a))))
+    (is (= 16 (* (cond (> a b) a
+                       (< a b) b
+                       :else -1)
+                 (+ a 1))))
+    )
   (testing "Exercise 1.2"
-           (is (= (/ -37 150)
-                  (/ (+ 5 4
-                        (- 2 (- 3 (+ 6 (/ 4 5)))))
-                     (* 3 (- 6 2) (- 2 7))))))
+    (is (= (/ -37 150)
+           (/ (+ 5 4
+                 (- 2 (- 3 (+ 6 (/ 4 5)))))
+              (* 3 (- 6 2) (- 2 7))))))
   (testing "Exercise 1.3"
-           (is (= 58
-                  (sum-of-squares-of-two-largest 3 7 1)))
-           (is (= 13
-                  (sum-of-squares-of-two-largest 1 2 3))))
+    (is (= 58
+           (sum-of-squares-of-two-largest 3 7 1)))
+    (is (= 13
+           (sum-of-squares-of-two-largest 1 2 3))))
   (testing "Exercise 1.4"
-           (is (= 8
-                  (a-plus-abs-b 3 -5)))
-           (is (= 10
-                  (a-plus-abs-b 5 5))))
+    (is (= 8
+           (a-plus-abs-b 3 -5)))
+    (is (= 10
+           (a-plus-abs-b 5 5))))
   (testing "Exercise 1.5"
-           (is (thrown? StackOverflowError (mytest 0 (p)))))
+    (is (thrown? StackOverflowError (mytest 0 (p)))))
 
   (testing "square roots"
-           (is (= 1.4142156862745097 (square-root 2))))
+    (is (= 1.4142156862745097 (square-root 2))))
   )
